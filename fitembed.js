9c52c03745a94704c321a6dca0a7ff9b02f62f7b
rajin98 <45932801+rajin98@users.noreply.github.com>
2020-01-07 21:08:59 -0700

    Add files via upload

--- fitembed.js
+++ fitembed.js
@@ -1,6 +1,6 @@
-var calc = document.getElementById('basic-calc-embed');
-var conv = document.getElementById('unit-conv-embed');
-var perc = document.getElementById('percentCalc-embed');
+var calc = document.getElementsByClassName('basic-calc-embed');
+var conv = document.getElementsByClassName('unit-conv-embed');
+var perc = document.getElementsByClassName('percentCalc-embed');
 
 var isMobile = 0;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -13,7 +13,11 @@ adapt();
 window.addEventListener("resize", adapt)
 
 function adapt(argument) {
-	if(calc){var width = calc.offsetWidth;calc.style.paddingBottom = (width>596) ? (width * 0.42 + "px") : (width * 1.2 + "px");}
+	if(calc.length > 0){
+		for (var i = calc.length - 1; i >= 0; i--) {
+			var width = calc[i].offsetWidth;calc[i].style.paddingBottom = (width>596) ? (width * 0.42 + "px") : (width * 1.2 + "px");}
+		}
+	 
 	if(conv){var width = conv.offsetWidth;conv.style.paddingBottom = (isMobile || width<596)? "330px":"480px";}
 	if(perc){var width = perc.offsetWidth;perc.style.paddingBottom = (width>596)? "260px":"395px";}
 }
