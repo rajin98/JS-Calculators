2e154a0719557978a8e0694e06211fc4c7a33de9
rajin98 <45932801+rajin98@users.noreply.github.com>
2020-01-07 20:12:59 -0700

    Add files via upload

--- fitembed.js
+++ fitembed.js
@@ -1,6 +1,6 @@
-var calc = document.getElementById('basic-calc-embed');
-var conv = document.getElementById('unit-conv-embed');
-var perc = document.getElementById('percentCalc-embed');
+var calc = document.getElementsById('basic-calc-embed');
+var conv = document.getElementsById('unit-conv-embed');
+var perc = document.getElementsById('percentCalc-embed');
 
 var isMobile = 0;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -13,7 +13,7 @@ adapt();
 window.addEventListener("resize", adapt)
 
 function adapt(argument) {
-	if(calc){var width = calc.offsetWidth;calc.style.paddingBottom = (width>576)? width * 0.415 + "px" : width * 1.16 + "px";}
+	if(calc){var width = calc.offsetWidth;calc.style.paddingBottom = (width>576) ? (width * 0.416 + "px") : (width * 1.2 + "px");}
 	if(conv){var width = conv.offsetWidth;conv.style.paddingBottom = (isMobile || width<576)? "330px":"480px";}
 	if(perc){var width = perc.offsetWidth;perc.style.paddingBottom = (width>576)? "260px":"395px";}
 }
