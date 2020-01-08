0da1a56c59a6de282f99cce7e2dd30a5a4236fba
rajin98 <45932801+rajin98@users.noreply.github.com>
2020-01-07 20:41:21 -0700

    Add files via upload

--- fitembed.js
+++ fitembed.js
@@ -13,8 +13,8 @@ adapt();
 window.addEventListener("resize", adapt)
 
 function adapt(argument) {
-	if(calc){var width = calc.offsetWidth;calc.style.paddingBottom = (width>576) ? (width * 0.416 + "px") : (width * 1.2 + "px");}
-	if(conv){var width = conv.offsetWidth;conv.style.paddingBottom = (isMobile || width<576)? "330px":"480px";}
-	if(perc){var width = perc.offsetWidth;perc.style.paddingBottom = (width>576)? "260px":"395px";}
+	if(calc){var width = calc.offsetWidth;calc.style.paddingBottom = (width>596) ? (width * 0.42 + "px") : (width * 1.2 + "px");}
+	if(conv){var width = conv.offsetWidth;conv.style.paddingBottom = (isMobile || width<596)? "330px":"480px";}
+	if(perc){var width = perc.offsetWidth;perc.style.paddingBottom = (width>596)? "260px":"395px";}
 }
 
